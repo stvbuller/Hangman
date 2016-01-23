00c320764ca0176f1ea2f4d8e96b22eb9bf7852b
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-01-23 16:50:25 -0500

    the wordRender function in word.js works

--- letter.js
+++ letter.js
@@ -4,7 +4,7 @@ function Letter(let) {
   this.charac = let;
   this.appear = false;
   this.letterRender = function() {
-    if (this.charac === undefined) {  //??? if charac === false 
+    if (this.appear === false) {  
       return "_";
     } else {
       return this.charac;
--- word.js
+++ word.js
@@ -1,37 +1,54 @@
 var Letter = require("./letter.js");
 module.exports = Word;
-debugger
+
 function Word(wrd) {
   this.word = wrd;
   this.lets = [];
   this.found = false;
   this.getsLets = function() {
-    //iterate over each letter in wrd property
-    for (var i = 0; i < this.word.length; i++) {               //iterate for the nubmer of character in the word 
-      this.lets[i] = new Letter(this.word.charAt(i));          //get the characters out of the word
+    //create Letter objects for each letter in the word
+    //and put the objects in an arry
+    for (var i = 0; i < this.word.length; i++) {               
+      this.lets[i] = new Letter(this.word.charAt(i));          
     }
   }
+  //check to see if the guess letter is in the word
   this.checkIfLetterFound = function(guessLetter) {
     this.whatToReturn = 0;
-    for (var i = 0; this.lets.length; i++){
-      if (Letter.charac === this.guessLetter) {
-        Letter.appear = true;
-        this.whatToReturn += 1;              //??? add 1 to checkIfLetterFound or whatToReturn
-        return checkIfLetterFound //??? or return whatToReturn
+    for (var i = 0; i < this.lets.length; i++){
+      if (guessLetter === this.lets[i].charac) {
+        this.lets[i].appear = true;
+        this.whatToReturn += 1;              
       }
     }
+    return this.whatToReturn; 
   }
-  // this.didWeFindTheWord = function() {
-  //   this.lets.every(function(curLets))  //??? anonymous function with argument curLets
-  //     // if all letter objects have the true value in their appear property, found = true
-  //       this.found = true;
-
-  //     return this.found;
-
-  // }
+  //array_name.every(function() { ….//yourcode });
+  this.didWeFindTheWord = function() {
+    // if all letter objects have the true value in their  
+    //appear property, found = true
+    // this.lets.every(function(curLets) {
+    //   if (curLets.appear === true){
+    //    this.found = true;
+    //    }
+    //})
+      return this.found;
+  }
+  this.wordRender = function() {
+    this.str = "";
+    var string1 = "";
+    for (var i = 0; i < this.lets.length; i++){
+       string1 = string1 + this.lets[i].letterRender();
+       this.str = string1;
+    }
+    return this.str;
+  }
 }
+debugger
 
-var word1 = new Word("one");
-word1.getsLets();
-
-
+//this are used to test the functions in word.js
+// var word1 = new Word("nnqn");
+// word1.getsLets();
+// var resultLetterCheck = word1.checkIfLetterFound("n");
+// var resultWordCheck = word1.didWeFindTheWord();
+// var resultWordRender = word1.wordRender();

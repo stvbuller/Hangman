ec1ddc53c63f17e2a0dbc2ba87bf5a45f51bbaa0
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-01-24 13:38:20 -0500

    calling wordRender from main.js works

--- main.js
+++ main.js
@@ -12,32 +12,32 @@ var game = function() {
     var startWord = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
     this.currentWrd = new Word(startWord);
     this.currentWrd.getLets();
-    keepPromptingUser();
+    this.keepPromptingUser();
   }
-  keepPromptingUser = function() {
+  this.keepPromptingUser = function() {
     var self = this;
     prompt.get(["guessLetter"], function(err, result){
       console.log("The letter or space you guessed is " + result.guessLetter);
-      //var findHowManyOfUserGuess = this.currentWrd.checkIfLetterFound(result.guessLetter);
-      var findHowManyOfUserGuess = 1;     //created this variable to test the game
+      var findHowManyOfUserGuess = self.currentWrd.checkIfLetterFound(result.guessLetter);
       if (findHowManyOfUserGuess === 0){
         console.log("You guessed wrong!");
-        this.guessesRemaining -= 1;
+        self.guessesRemaining -= 1;
       } else {
         console.log("You guessed right!")
-        // if (this.currentWrd.didWeFindTheWord() === true) {
+        // if (self.currentWrd.didWeFindTheWord() === true) {
         //   console.log("You won!")
         //   return 1;
         // }
       }
-      console.log("Guesses remaining: " + this.guessesRemaining);
-      //console.log(this.currentWrd.wordRender());
-      if (this.guessesRemaining === 0 && this.currentWrd === false){
-        keepPromptingUser();
+      console.log("Guesses remaining: " + self.guessesRemaining);
+      console.log(self.currentWrd.wordRender());
+      if (self.guessesRemaining > 0 && self.currentWrd === false){
+        self.keepPromptingUser();        
       } else if (this.guessesRemaining === 0){ 
         console.log("Game over bro " + currentWrd);
       } else {
-        //console.log(this.currentWrd.wordRender());
+        console.log(self.currentWrd.wordRender());
+        self.keepPromptingUser();
       }
     }); 
   } 
@@ -46,10 +46,10 @@ var game = function() {
 
 //these are used to check variables in the game
 var game1 = new game();
-var resultWordBank = game1.wordBank;
-var wordBankLength = game1.wordBank.length;
-var resultGuessesRemaining = game1.guessesRemaining;
 game1.startGame();
-var resultsLets = game1.currentWrd;
+// var resultWordBank = game1.wordBank;
+// var wordBankLength = game1.wordBank.length;
+// var resultGuessesRemaining = game1.guessesRemaining;
+// var resultsLets = game1.currentWrd;
 
 

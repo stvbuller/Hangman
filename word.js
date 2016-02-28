88094924475a7ba11e96dd55c19dba956d5f4702
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-02-27 20:00:58 -0500

    added a didWeFindTheWord function that works

--- word.js
+++ word.js
@@ -8,8 +8,8 @@ function Word(wrd) {
   this.getLets = function() {
     //create Letter objects for each letter in the word
     //and put the objects in an arry
-    for (var i = 0; i < this.word.length; i++) {              
-      this.lets[i] = new Letter(this.word.charAt(i));          
+    for (var i = 0; i < this.word.length; i++) {
+      this.lets[i] = new Letter(this.word.charAt(i));
     }
   }
   //check to see if the guess letter is in the word
@@ -18,41 +18,35 @@ function Word(wrd) {
     for (var i = 0; i < this.lets.length; i++){
       if (guessLetter === this.lets[i].charac) {
         this.lets[i].appear = true;
-        this.whatToReturn += 1;              
+        this.whatToReturn += 1;
       }
     }
-    return this.whatToReturn; 
+    return this.whatToReturn;
   }
   //*******  didWeFindTheWord is not working
   //if the appear property of all letters is equal to true set found = true
-  this.didWeFindTheWord = function() {
-    //array_name.every(function() { ….//yourcode });
-    this.lets.every(function(curLets) {
-      if (curLets.appear == true){
-        this.found = true;
-      }
-    });
-    return this.found;
-  }
-//*** alternate didWeFindTheWord function created by ajurado2204
-  // this.didWeFindTheWord = function(){
-  //   var result = this.lets.every(function(curLet){
-  //     if(curLet.appear === true){
-  //       curLet.found = true;
-  //       return curLet.found;
+  // this.didWeFindTheWord = function() {
+  //   //array_name.every(function() { ….//yourcode });
+  //   this.lets.every(function(curLets) {
+  //     if (curLets.appear == true){
+  //       this.found = true;
   //     }
   //   });
+  //   return this.found;
+  // }
 
+
+  //*** alternate didWeFindTheWord function created by ajurado2204
+  this.didWeFindTheWord = function(){
+    var result = this.lets.every(function(curLet){
+      if(curLet.appear === true){
+        curLet.found = true;
+        return curLet.found;
+      }
+    });
     return result;
   }
-  //*** alternate didWeFindTheWord function suggested by TA
-  //*** curLets is not defined error
-  // this.didWeFindTheWord = function() {
-  //   this.found = this.lets.every(function(curLet) {
-  //     return curLets.appear; // console.log(this.found) "true"
-  //     });
-  //   return this.found // console.log(this.found) "true"
-  // }
+
   //create a string of the characters guessed in the array using letterRender()
   this.wordRender = function() {
     this.str = "";
